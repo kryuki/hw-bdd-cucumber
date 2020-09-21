c98c7810a2452f4db9cbbc281959bce5f289139c
EC2 Default User <ec2-user@ip-172-31-1-78.us-east-2.compute.internal>
2020-09-21 10:57:26 +0000

    project complete

--- rottenpotatoes/features/step_definitions/movie_steps.rb
+++ rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -4,8 +4,9 @@ Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
+    Movie.create!(movie)
   end
-  fail "Unimplemented"
+  #fail "Unimplemented"
 end
 
 Then /(.*) seed movies should exist/ do | n_seeds |
@@ -15,10 +16,21 @@ end
 # Make sure that one string (regexp) occurs before or after another one
 #   on the same page
 
-Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
+
+
+Then /I should see \"(.*)\" before \"(.*)\"/ do |e1, e2|
+  
   #  ensure that that e1 occurs before e2.
   #  page.body is the entire content of the page as a string.
-  fail "Unimplemented"
+  #binding.pry
+  
+  #e1.should be e1
+  (!!(page.body.gsub(/[\r\n]/,"") =~ /#{e1}.*#{e2}/i)).should be (e1 == e1)
+  #assert page.body.gsub(/[\r\n]/,"") =~ /#{e1}.*#{e2}/
+  #puts check
+  #assert check
+  # assert check =~ /#{e2}/
+  #assert page.body =~ /#{e1}.*#{e2}/m
 end
 
 # Make it easier to express checking or unchecking several boxes at once
@@ -29,10 +41,19 @@ When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
-  fail "Unimplemented"
+  #fail "Unimplemented"
+  rating_list.split.reverse.each do |el|
+    el.strip!
+    if uncheck
+      uncheck("ratings_#{el}")
+    else
+      check("ratings_#{el}")
+    end
+  end
 end
 
 Then /I should see all the movies/ do
   # Make sure that all the movies in the app are visible in the table
-  fail "Unimplemented"
+  #fail "Unimplemented"
+  page.all('tbody tr').count.should == Movie.count
 end
